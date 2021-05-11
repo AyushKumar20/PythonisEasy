5f2707d3ff19a480a698d4d6a3dced3569f0474f
AyushKumar20 <77678837+AyushKumar20@users.noreply.github.com>
2021-05-11 15:27:53 +0530

    Add files via upload

---
+++ Homework#1.py
@@ -0,0 +1,36 @@
+SongName = "Kya Tum Naraaz Ho?"
+Artist = "Tanmaya Bhatnagar"
+LyricsAndComposition = "Tanmaya Bhatnagar"          #Lyrics & Composition done by
+Genre = "Rhythm and blues"
+Duration = 3.32                                     #Duration in min
+DurationInSeconds = 212                             #Duration in sec
+ReleaseDate = "April 10, 2020"
+Album = "Kya Tum Naraaz Ho?(Raw)"
+MusicVideo = "No"
+SampledIn = "Tanmaya Bhatnagar - Raat Adhoori"      #Song sampled in another song of same artist
+TempoBPM = 97                                       #Tempo of the track is beats per minute
+LoudnessDB = -16.51                                 #This is average loudness of the track in decibles(db)
+Key = "c# Major"
+Explicit = "No"
+Studio = "None"
+Distributor = "CD Baby"                             #Digital Music Distribution
+AvailableOn = "Apple Music, Spotify, Youtube"       #Availibility of music on platforms
+ViewsOnYT = 157730                                  #Views on Youtube
+print(SongName)
+print(Artist)
+print(LyricsAndComposition)
+print(Genre)
+print(Duration)
+print(DurationInSeconds)
+print(ReleaseDate)
+print(Album)
+print(MusicVideo)
+print(SampledIn)
+print(TempoBPM)
+print(LoudnessDB)
+print(Key)
+print(Explicit)
+print(Studio)
+print(Distributor)
+print(AvailableOn)
+print(ViewsOnYT)
